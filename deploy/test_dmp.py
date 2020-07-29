ff1bd0959bdf0096a80c8e4c306744457163d140
atai2 <1114677814@qq.com>
2020-07-29 23:42:33 +0800

    tt

--- deploy/test_dmp.py
+++ deploy/test_dmp.py
@@ -2,9 +2,12 @@
 # -*- coding:utf-8 -*-
 
 import requests
+from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-
-    post = requests.post("http://localhost:8000/getAgents")
-
+    post = requests.get("http://localhost:8080")
+    soup = BeautifulSoup(post.text)
+    for i in soup.find_all("li"):
+        item =i.text
+        print(item.replace("/",""))
     print(post.text)
