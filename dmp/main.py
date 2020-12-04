9fdabac8d09084344fb8973f869ba6b497175a67
wangtaihe <wangtaihe@inspur.com>
2020-12-04 10:20:05 +0800

    setting info

--- dmp/main.py
+++ dmp/main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # author:wangtaihe
 # datetime:2020/12/3 15:05
 # software: PyCharm
 
 
-import os,pytest
-if __name__ == '__main__':
+import os, pytest
 
-    pytest.main(["--html=./report.html"])
+if __name__ == '__main__':
+    # pytest.main(["--html=./report.html"])
+    pytest.main(["--count=10", "--html=./report.html"])
     # os.system("py.test  --html=./report.html")
     # os.system("py.test test_report.py --html=./report.html")
     # os.system("py.test test_setting.py --html=./report.html")
